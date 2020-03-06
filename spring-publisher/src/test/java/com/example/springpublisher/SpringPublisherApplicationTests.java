b6b28fdf31b93277c94278ad1bfc467d190388fd
Daniel Pelaez <estigma88@gmail.com>
2020-03-05 21:48:18 -0500

    Adding first structure

---
+++ spring-publisher/src/test/java/com/example/springpublisher/SpringPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPublisherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
